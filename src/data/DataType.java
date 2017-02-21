c372bad76fcf361949f4048ffd455ffd9ca10b19
Isabella <Isabella@Isabella>
2017-02-21 16:53:28 +0100

    samt apache

--- src/data/DataType.java
+++ src/data/DataType.java
@@ -48,30 +48,3 @@ public class DataType {
     }
 
 }
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package data;
-
-import java.util.LinkedList;
-
-/**
-* C.G
- */
-public class DataType {
-    private LinkedList<Extension> extensionlist;
-    public DataType () {
-        extensionlist = new LinkedList<>();
-    }
-
-    void removeExtension(Extension aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void addExtension(Extension aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-}
